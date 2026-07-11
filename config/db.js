db485fdc9c7b71ec3e4d06970a82e3f371071065
ishimdar <ishimdar.ahamad@gmail.com>
2026-07-11 23:56:51 +0530

    Fixed the validate certificate

--- config/db.js
+++ config/db.js
@@ -8,7 +8,8 @@ const connectDb = async () => {
         // await mongoose.connect(MONGO_URI);
         await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
-            useUnifiedTopology: true
+            useUnifiedTopology: true,
+            tlsAllowInvalidCertificates: true
         });
 
         // await mongoose.connect(MONGO_URI, {
